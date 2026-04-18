8de0fadffdae4f762f0815ae86db9d9136aca28f
Darrel Griët <dgriet@gmail.com>
2026-04-18 21:02:26 +0200

    wip: shader in node
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

--- src/controls/src/flatmeshnode.h
+++ src/controls/src/flatmeshnode.h
@@ -72,6 +72,8 @@ private:
     int m_unitWidth, m_unitHeight;
     QColor m_centerColor, m_outerColor;
     QQuickWindow *m_window;
+    int m_loopCount;
+    float m_screenScaleFactor;
     Point m_points[NUM_POINTS_X*NUM_POINTS_Y];
 };
 
